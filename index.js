8f86d0db7b487663a20ae84d42c45ca0ac153a98
Koppula <monikk@ushydmonikkmb.us.deloitte.com>
2019-05-20 12:41:09 +0530

    Remove cool thing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cool = require('cool-ascii-faces')
+// const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,5 +8,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  // .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
